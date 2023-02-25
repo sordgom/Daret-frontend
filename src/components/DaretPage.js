2c312b19d09366c2cab4ca516a9a8d18db98d79b
sordgom <aymaneigmiden@gmail.com>
2023-02-25 13:34:36 +0800

    - fix the provider/signer to get them straight from magic 			- Look into long loading(they're sc errors ) => check remix 			- added role based button

--- src/components/DaretPage.js
+++ src/components/DaretPage.js
@@ -4,30 +4,65 @@ import Web3 from "web3";
 import {magic} from '../lib/magicConnect';
 import { UserContext } from '../lib/UserContext';
 import { Container, Row, Col, Button } from "react-bootstrap";
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import 'animate.css';
 
 export const DaretPage = () => {
   let { address } = useParams();
+  let navigate = useNavigate();
+
   const web3 = new Web3(magic.rpcProvider);
   const [user, setUser] = useContext(UserContext);
   // Round list variable
   const [round, setRound] = useState(null);
+  const [owner, setOwner] = useState(null);
   // Contract
   const contract = new web3.eth.Contract(  DARET_CONTRACT_ABI, address, { from: user });
 
   useEffect(() => {
-        getProperties();        
-        console.log(round)
+        getProperties();    
+        getOwner();   
+        removeFromDb(); 
     }, [user]); 
 
     const getProperties = async () => {
         try {
-            let a = await contract.methods.rounds(1).call()
+            let a = await contract.methods.rounds(1).call();
             setRound(a);
         } catch (error) {
             console.error(error);
         }
+    };
+
+    const getOwner = async () => {
+      try {
+          let a = await contract.methods.owner().call();
+          setOwner(a);
+      } catch (error) {
+          console.error(error);
+      }
+    };
+
+    const removeFromDb = async () => {
+      try {
+          let a = await contract.methods.state().call();
+          if(a==3)
+          {
+            fetch('http://localhost:8080/daret/'+address, {
+              method: 'DELETE',
+            })
+            .then(response => response.json())
+            .then(data => {
+              console.log('Success:', data);
+              navigate('/daret');
+            })
+            .catch((error) => {
+              console.error('Error:', error);
+            });
+          }
+        } catch (error) {
+          console.error(error);
+      }
     };
 
     const start = async () => {
@@ -45,11 +80,13 @@ export const DaretPage = () => {
       }
     };
 
+    //Must meet the min contribution of 1000000 wei
     const join  = async () => {
       try {
         await contract.methods.joinRound()
         .send({
-          from: user
+          from: user,
+          value: 1000000
         })
         .on('receipt', function(receipt){
             // receipt example
@@ -60,11 +97,13 @@ export const DaretPage = () => {
       }
     };
 
+    //Must meet the min contribution of 1000000 wei && already joined
     const contribute  = async () => {
       try {
         await contract.methods.addContribution()
         .send({
-          from: user
+          from: user,
+          value: 1000000
         })
         .on('receipt', function(receipt){
             // receipt example
@@ -75,9 +114,13 @@ export const DaretPage = () => {
       }
     };
 
+    //Check requirements:
+    //1. Round must be closed
+    //2. total contributions must be greater than payout...
+    
     const complete  = async () => {
       try {
-        await contract.methods.completeRound()
+        await contract.methods.completeRound(user)
         .send({
           from: user
         })
@@ -113,21 +156,27 @@ export const DaretPage = () => {
             <Col size={12}>
                 <div className="">
                     <h3>Daret</h3>
-                    <Button onClick={start}>
-                        Start Round
-                    </Button>
-                    <Button onClick={join}>
-                        Join Round
-                    </Button>
-                    <Button onClick={contribute}>
-                        Contribute
-                    </Button>
-                    <Button onClick={close}>
-                        Close Round
-                    </Button>
-                    <Button onClick={complete}>
-                        Complete Round
-                    </Button>
+                      {owner == user && 
+                      <Row>
+                        <Button onClick={start}>
+                            Start Round
+                        </Button>
+                        <Button onClick={complete}>
+                            Complete Round
+                        </Button>
+                        <Button onClick={close}>
+                            Close Round
+                        </Button>
+                      </Row>
+                        }
+                    <Row>
+                      <Button onClick={join}>
+                          Join Round
+                      </Button>
+                      <Button onClick={contribute}>
+                          Contribute
+                      </Button>
+                    </Row>
                 </div>
             </Col>
           </Row>
