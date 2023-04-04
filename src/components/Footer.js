72f70e4871b229fb2fb713462385596d6269c984
sordgom <aymaneigmiden@gmail.com>
2023-04-04 10:05:24 +0800

    fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,9 +15,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-                <a href="https://discord.com/users/400084054852239361" target="_blank"><img src={navIcon3} alt="Discord" /></a>
+                <a href="https://discord.com/users/400084054852239361" target="_blank"><img src={navIcon1} alt="Discord" /></a>
                 <a href="https://github.com/sordgom"target="_blank"><img src={navIcon2} alt="Github" /></a>
-                <a href="https://twitter.com/sordgom" target="_blank"><img src={navIcon1} alt="Twitter" /></a>
+                <a href="https://twitter.com/sordgom" target="_blank"><img src={navIcon3} alt="Twitter" /></a>
             </div>
           </Col>
         </Row>
